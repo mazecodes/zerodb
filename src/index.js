670a9620a4948bf1412ff1a82cc9b8ecc30ca4f0
Maze Peterson <mazecodes@protonmail.com>
2021-01-30 07:11:06 -0800

    Create ZeroDB class

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+
+class ZeroDB {
+  constructor(source) {
+    this.source = source;
+    this.database = {};
+
+    if (!this.source) {
+      throw new Error('Database source should be provided');
+    }
+
+    if (typeof this.source !== 'string') {
+      throw new Error('Database source should be a valid string');
+    }
+
+    if (path.extname(this.source) !== '.json') {
+      throw new Error('Database source should be JSON');
+    }
+
+    const filePath = path.resolve(require.main.path, this.source);
+    const fileExists = fs.existsSync(filePath);
+
+    if (!fileExists) {
+      fs.writeFileSync(filePath, '{}');
+    } else {
+      try {
+        const data = fs.readFileSync(filePath);
+        const database = JSON.parse(data);
+
+        this.database = database;
+      } catch (err) {
+        throw new Error('Database source contains malformed JSON');
+      }
+    }
+  }
+}
+
+module.exports = ZeroDB;
