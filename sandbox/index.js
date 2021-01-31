43ae5f6c9fca1a429d7cd08bea2a34c1ac02c75f
Maze Peterson <mazecodes@protonmail.com>
2021-01-31 09:11:44 -0800

    Organize the sandbox

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,11 +1,61 @@
+const { zipObjectDeep } = require('lodash');
 const ZeroDB = require('../src');
 
-const zerodb = new ZeroDB('./database.json', {
+const zdb = new ZeroDB('./database.json', {
   encryption: true,
-  secret: 'hello WOrld',
-  // empty: true,
+  secret: 's3cr3t',
 });
 
-// zerodb.init({ username: 'Hello World' }).reset().save();
+zdb.init({ posts: [], user: {} }, { force: true });
 
-console.log(zerodb.getState());
+zdb.set('user.name', 'John Doe');
+
+zdb
+  .push('posts', {
+    id: 0,
+    title: 'Title 1',
+  })
+  .push('posts', {
+    id: 1,
+    title: 'Title 2',
+  })
+  .push('posts', {
+    id: 2,
+    title: 'Title 3',
+  });
+
+console.log(zdb.get('user.name'));
+
+const post = zdb.findOne('posts', { id: 0 });
+
+console.log(post);
+
+console.log(zdb.has('posts'));
+
+zdb.delete('posts');
+zdb.reset();
+
+zdb.set('user.age', 18);
+zdb.increment('user.age');
+zdb.increment('user.age', 10);
+zdb.decrement('user.age');
+zdb.decrement('user.age', 5);
+
+console.log(zdb.get('user.age'));
+
+zdb.set('user.name', 'John Doe');
+
+zdb.update('user.name', name => name.toLowerCase());
+console.log(zdb.get('user.name'));
+
+console.log(zdb.getState());
+
+zdb
+  .setState({
+    posts: [
+      {
+        title: 'Test',
+      },
+    ],
+  })
+  .save(() => console.log('Saved'));
